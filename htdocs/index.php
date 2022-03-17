53202214a9dec950e7a35c974e746f2ac3866d6d
tenzap <fabstz-it@yahoo.fr>
2022-03-17 15:39:46 +0100

    rearrange homepage, update facebook iframe

--- htdocs/index.php
+++ htdocs/index.php
@@ -20,11 +20,30 @@ $size_tar_zip = /*filesize($file_path.$file_zip);*/ '1.2MB';
 <div id="wrap">
 	<div id="shadow"></div>
 	<div id="content">
-		<div id="testimonial" style="padding: 3px;"><i>&#34;I think Kalkun is great because it is easy to configure, easy to customize, easy to use, and any target hardware used.&#34;</i>&nbsp;(<b>Simon Nandor</b> - <a href="https://www.simonszoft.hu/">Software Engineer</a>).
+		<div style="display:table">
+		<div style="display:table-cell">
+			<h3 >Manage your SMS easily</h3>
+			<p>Kalkun is open source web-based SMS (<i>Short Message Service</i>) manager.</p>
+			<p>It uses <a href="https://wammu.eu/smsd/">Gammu SMSD</a> as SMS gateway engine to deliver and retrieve messages from your phone/modem.</p>
+			<p><a href="about.php">Learn more about Kalkun...</a></p>
+		</div>
+		<div style="display:table-cell; vertical-align: middle;">
+			<div id="dl-button">
+				Download Kalkun<br />
+				<a href="https://github.com/kalkun-sms/Kalkun/releases/latest"><small>latest release</small></a> |
+				<a href="https://github.com/kalkun-sms/Kalkun/archive/refs/heads/devel.tar.gz"><small>devel snapshot</small></a>
+			</div>
+			<div style="text-align:center;"><a href="https://github.com/kalkun-sms/Kalkun/wiki/Changelog">Changelog</a> | <a href="https://github.com/kalkun-sms/Kalkun/wiki/Release-notes">Release notes</a></div>
+			<div>&nbsp;</div>
+		</div>
 		</div>
 
 		<div id="main">
-		<a href="screenshots.php" title="More Screenshots"><img id="screen" src="media/images/2022-03/02-dashboard.png" /></a>
+		<a href="screenshots.php" title="More Screenshots"><img id="screen" src="media/images/2022-03/02-dashboard.png" />
+		</a>
+		<h3>Testimonial</h3>
+			<div id="testimonial" style="padding: 3px;"><i>&#34;I think Kalkun is great because it is easy to configure, easy to customize, easy to use, and any target hardware used.&#34;</i>&nbsp;(<b>Simon Nandor</b> - <a href="https://www.simonszoft.hu/">Software Engineer</a>).
+			</div>
 
 		<div id="news" style="display: none;">
 		<h3>News</h3>
@@ -33,18 +52,6 @@ $size_tar_zip = /*filesize($file_path.$file_zip);*/ '1.2MB';
 		</div>
 
 		<div id="sidebar">
-			<h3>Manage your SMS easily</h3>
-			<p>Kalkun is open source web-based SMS (<i>Short Message Service</i>) manager.</p>
-			<p>It uses <a href="https://wammu.eu/smsd/">Gammu SMSD</a> as SMS gateway engine to deliver and retrieve messages from your phone/modem.</p>
-			<p><a href="about.php">Learn more about Kalkun...</a></p>
-
-			<div id="dl-button">
-				Download Kalkun<br />
-				<a href="https://github.com/kalkun-sms/Kalkun/releases/latest"><small>latest release</small></a> |
-				<a href="https://github.com/kalkun-sms/Kalkun/archive/refs/heads/devel.tar.gz"><small>devel snapshot</small></a>
-			</div>
-			<div style="text-align:center;"><a href="https://github.com/kalkun-sms/Kalkun/wiki/Changelog">Changelog</a> | <a href="https://github.com/kalkun-sms/Kalkun/wiki/Release-notes">Release notes</a></div>
-			<div>&nbsp;</div>
 
 			<div>
 			<strong>Features</strong>
@@ -67,7 +74,7 @@ $size_tar_zip = /*filesize($file_path.$file_zip);*/ '1.2MB';
 
 			<div>&nbsp;</div>
 			<div>
-			<iframe src="https://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FKalkun%2F136612446404796&amp;width=240&amp;colorscheme=light&amp;show_faces=true&amp;stream=true&amp;header=true&amp;height=427" style="border:none; overflow:hidden; width:240px; height:427px;" allowTransparency="true"></iframe>
+			<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FKalkun-136612446404796%2F&tabs=fb-page&width=225&height=300&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="225" height="300" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
 			</div>
 		</div>
 		<div style="clear: both">&nbsp;</div>
